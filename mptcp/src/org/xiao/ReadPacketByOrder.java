f2694a7cf6e0dd523f69a34363ab1c590a378156
hebo <hebo@gmail.com>
2017-10-30 09:25:15 +0800

    循环检测数据包并将特征值写入TXT文件

--- mptcp/src/org/xiao/ReadPacketByOrder.java
+++ mptcp/src/org/xiao/ReadPacketByOrder.java
@@ -1,11 +1,9 @@
 package org.xiao;
 
-import java.io.EOFException;
+import java.io.*;
 import java.net.Inet4Address;
-import java.util.ArrayList;
+import java.text.DecimalFormat;
 import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
 import java.util.concurrent.TimeoutException;
 import org.pcap4j.core.NotOpenException;
 import org.pcap4j.core.PcapHandle;
@@ -19,18 +17,27 @@ import org.pcap4j.packet.UdpPacket;
 public class ReadPacketByOrder {
     private static final String PCAP_FILE_KEY
             = ReadPacketFile.class.getName() + ".pcapFile";
-    private static final String FILE="xunlei_";
-    private static int ORDER=18;
-    private static int COUNT=21;
+    private static final String FILE="AHD_";
+    private static int ORDER=0;//起始文件标号
+    private static int COUNT=65;//循环检测的pcap文件数量
     private static String PCAP_FILE
-            = System.getProperty(PCAP_FILE_KEY, "H:/xunlei/"+FILE+ORDER+"_down.pcap");
+            = System.getProperty(PCAP_FILE_KEY, "H:/AHD/"+FILE+"down_"+ORDER+".pcap");
     private static String PCAP_FILE1
-            = System.getProperty(PCAP_FILE_KEY, "H:/xunlei/"+FILE+ORDER+"_up.pcap");
+            = System.getProperty(PCAP_FILE_KEY, "H:/AHD/"+FILE+"up_"+ORDER+".pcap");
     private ReadPacketByOrder() {}
     public static void main(String[] args) throws PcapNativeException, NotOpenException {
+        String filename = "AHD_output.txt";
+        File file = new File(filename);
+        PrintWriter writer = null;
+        DecimalFormat df = new DecimalFormat("###.000000");
+        try {
+            writer = new PrintWriter(new FileOutputStream(file));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
         for (int n = 0; n < COUNT; n++) {
-            PCAP_FILE = System.getProperty(PCAP_FILE_KEY, "H:/xunlei/"+FILE+ORDER+"_down.pcap");
-            PCAP_FILE1 = System.getProperty(PCAP_FILE_KEY, "H:/xunlei/"+FILE+ORDER+"_up.pcap");
+            PCAP_FILE = System.getProperty(PCAP_FILE_KEY, "H:/AHD/"+FILE+"down_"+ORDER+".pcap");
+            PCAP_FILE1 = System.getProperty(PCAP_FILE_KEY, "H:/AHD/"+FILE+"up_"+ORDER+".pcap");
             PcapHandle handle;
             PcapHandle handle1;
             double sum = 0;
@@ -52,6 +59,7 @@ public class ReadPacketByOrder {
                 handle1 = Pcaps.openOffline(PCAP_FILE1);
             }
             Inet4Address inet4AddressPre = handle.getNextPacket().get(IpV4Packet.class).getHeader().getSrcAddr();
+            //计算下行流的各项指标
             while (flag) {
                 try {
                     i++;
@@ -62,9 +70,6 @@ public class ReadPacketByOrder {
                         firsttample = (double) handle.getTimestampInts() * 1000000 + handle.getTimestampMicros();
                         subcount++;
                     }
-                    //                if (i == COUNT-1){
-                    //                    lasttample=(double)handle.getTimestampInts()*1000000+handle.getTimestampMicros();
-                    //                }
                     lasttample = (double) handle.getTimestampInts() * 1000000 + handle.getTimestampMicros();
                     //                UdpPacket udpPacket = packet.get(UdpPacket.class);
                     if (!inet4Address.equals(inet4AddressPre)) {
@@ -74,13 +79,13 @@ public class ReadPacketByOrder {
                     len = ipV4Packet.getHeader().getTotalLengthAsInt() + 14 + 16;
                     pck_cnt[(int) len] = pck_cnt[(int) len] + 1;
                     sum = sum + len;
-                    //                System.out.println(packet);
-                    //                System.out.println(ipV4Packet.getHeader().getTotalLengthAsInt());
-                    //                System.out.println(udpPacket.getHeader().getDstPort());
-                    //                System.out.println(handle.getTimestampMicros());
+//                  System.out.println(packet);
+//                  System.out.println(ipV4Packet.getHeader().getTotalLengthAsInt());
+//                  System.out.println(udpPacket.getHeader().getDstPort());
+//                  System.out.println(handle.getTimestampMicros());
                 } catch (TimeoutException e) {
                 } catch (EOFException e) {
-                    System.out.println("END");
+//                    System.out.println("END");
                     break;
                 }catch (IllegalArgumentException e){
                     System.out.println("AAA");
@@ -96,26 +101,38 @@ public class ReadPacketByOrder {
                     sum1 = sum1 + len1;
                 } catch (TimeoutException e) {
                 } catch (EOFException e) {
-                    System.out.println("END");
+//                    System.out.println("END");
                     break;
                 }catch (IllegalArgumentException e){
-                    System.out.println("AAA");
                     continue;
                 }
             }
+            //计算下行流的熵
             for (int j = 0; j < 1600; j++) {
                 if (pck_cnt[j] != 0) {
                     result = result - ((double) pck_cnt[j] / i) * (Math.log((double) pck_cnt[j] / i) / Math.log((double) 2));
-                    //                System.out.println(pck_cnt[j]);
+//                  System.out.println(pck_cnt[j]);
                 }
             }
-            System.out.println(ORDER);
-            System.out.println("比值：" + sum / sum1);
-            System.out.println("熵：" + result);
-            System.out.println("下行流分段数：" + Math.log(subcount));
-            System.out.println("平均到达时间间隔：" + (lasttample - firsttample) / i + "微秒");//4.8186777E7,1.306811573E9,7.0507805E7,1.337192329E9,8.1663159E7,1.352382707E9
+            writer.print(df.format(sum / sum1));
+            writer.print(' ');
+            writer.print(df.format((lasttample - firsttample) / i));
+            writer.print(' ');
+            writer.print(df.format(result));
+            writer.print(' ');
+            writer.print(df.format(Math.log(subcount)));
+            writer.println();
+            System.out.print(ORDER);
+            System.out.print("比值：" + sum / sum1);
+            System.out.print("熵：" + result);
+            System.out.print("下行流分段数：" + Math.log(subcount));
+            System.out.print("平均到达时间间隔：" + (lasttample - firsttample) / i + "微秒");//4.8186777E7,1.306811573E9,7.0507805E7,1.337192329E9,8.1663159E7,1.352382707E9
+            System.out.println(' ');
             handle.close();
             ORDER++;
+        }
+        if (writer != null){
+            writer.close();
         }
     }
 }
